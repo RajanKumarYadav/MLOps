6dd76d8f927d397aaed0c54977c5e8f20055feee
Rajan <rajan.kumar@live.in>
2024-08-10 17:44:56 +0530

    Session_7_Lab

---
+++ Session_7_Lab/README.md
@@ -0,0 +1,25 @@
+## Setting up Manual ML Project Structure
+
+```bash
+python template.py
+```
+
+## Setup Python ML Project using Cookiecutter
+
+### Documentation Link: https://cookiecutter.readthedocs.io/en/stable/
+
+### Installation
+```bash
+pip install cookiecutter 
+```
+### Create Github directory template
+```bash
+cookiecutter gh:audreyfeldroy/cookiecutter-pypackage  
+```
+
+## Logging & Exception
+
+
+### Host and load the model to MLFlow Server 
+
+
---
+++ Session_7_Lab/src/MLOPs_project_auto_template_exception_and_logging/app/app.py
@@ -0,0 +1,31 @@
+from src.MLOPs_project_auto_template_exception_and_logging.logger import logging
+from src.MLOPs_project_auto_template_exception_and_logging.exception import CustomException
+
+import sys
+
+# Importing the Library
+import pandas as pd
+from flask import request, Flask, jsonify
+import requests
+import json
+
+# Initialize Flask application
+app = Flask(__name__)
+
+
+@app.route('/diabetesPrediction', methods=['POST'])
+def diabetesPrediction():
+
+    try:
+        # Get JSON data from the request
+        data = request.json
+        
+        # Use of Logger 
+        logging.info("The Flask Application has started")
+
+    except Exception as e:
+        logging.info("Custom Exception")
+        raise CustomException(e,sys)
+
+# Run the Flask application
+app.run(debug=True, host="0.0.0.0", port=5010)
